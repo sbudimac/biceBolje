93f36c33d7ac513e78a78a2c9b438f1a26aa4658
sbudimac <sbudimac@gmail.com>
2020-10-21 22:00:25 +0200

    Kreirana klasa Entitet

---
+++ specifikacija/src/main/java/api/Entitet.java
@@ -0,0 +1,49 @@
+package api;
+
+import java.util.HashMap;
+
+public class Entitet {
+	private String naziv;
+	private String id;
+	private HashMap<String, Object> atributi;
+	
+	public Entitet() {};
+	
+	public Entitet(String naziv, String id) {
+		this.naziv=naziv;
+		this.id=id;
+		atributi=new HashMap<String, Object>();
+	}
+	
+	public String getNaziv() {
+		return naziv;
+	}
+	
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public HashMap<String, Object> getAtributi() {
+		return atributi;
+	}
+	
+	public void setAtributi(HashMap<String, Object> atributi) {
+		this.atributi = atributi;
+	}
+	
+	public void dodajAtribut(String key, Object value) {
+		atributi.put(key, value);
+	}
+	
+	public void obrisiAtribut(String key) {
+		atributi.remove(key);
+	}
+}
