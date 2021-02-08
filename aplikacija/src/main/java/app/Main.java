e07033c00872cd107ae8171f1ec98af252fafae8
Andrej Gasic <andrej@gasic.rs>
2021-02-08 08:30:22 +0100

    Brisanje starog main koda i centralizovanje gitignore

--- aplikacija/src/main/java/app/Main.java
+++ aplikacija/src/main/java/app/Main.java
@@ -1,9 +1,5 @@
 package app;
 
-import api.Entitet;
-import api.FileOperator;
-import api.Skladiste;
-import crudCustom.CustomOperator;
 import gui.MainView;
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -12,35 +8,6 @@ public class Main extends Application {
 
 	public static void main(String[] args) {
 		launch(args);
-		
-		/*Skladiste skladiste = new Skladiste();
-		skladiste.setOperator(new CustomOperator("C:\\Users\\andro\\OneDrive\\Desktop\\testiranjeCUSTOM"));
-        Entitet gupsi=new Entitet("Gupsi", "ooooo");
-        gupsi.setAtribut("Indeks", "RN3/18");
-        gupsi.setAtribut("Smer", "Racunarske nauke");
-        gupsi.setAtribut("Ocena", "5");
-        Entitet buda=new Entitet("Buda", "meathead");
-        buda.setAtribut("Indeks", "RN5/18");
-        buda.setAtribut("Smer", "Racunarske nauke");
-        buda.setAtribut("Ocena", "9");
-        Entitet gale=new Entitet("Gale", "stipendista");
-        gale.setAtribut("Indeks", "RN2/18");
-        gale.setAtribut("Smer", "Racunarske nauke");
-        gale.setAtribut("Ocena", "10");
-        Entitet cmoki=new Entitet("Cmoki", "gospoda");
-        cmoki.dodajUgnjezdeni("tim1", gupsi);
-        cmoki.dodajUgnjezdeni("tim2", buda);
-        cmoki.setAtribut("Indeks", "RI4/18");
-        cmoki.setAtribut("Smer", "Racunarsko inzenjerstvo");
-        cmoki.setAtribut("Ocena", "6");
-
-        skladiste.dodajEntitet(gale);
-        skladiste.dodajEntitet(cmoki);
-		
-		FileOperator fileOperator = (FileOperator)skladiste.getOperator();
-		for(Entitet e : skladiste.getEntiteti()) {
-			System.out.println(fileOperator.prevediEntitet(e));
-		}*/
 	}
 
 	@Override
